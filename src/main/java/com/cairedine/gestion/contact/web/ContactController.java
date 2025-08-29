cf0272b42e465adbfcb70ad61feedefe61ce1d79
CkalaiGit <cairedine@gmail.com>
2025-08-29 23:56:31 +0200

    ajout d'un layout

--- src/main/java/com/cairedine/gestion/contact/web/ContactController.java
+++ src/main/java/com/cairedine/gestion/contact/web/ContactController.java
@@ -1,20 +1,18 @@
 package com.cairedine.gestion.contact.web;
 
 import com.cairedine.gestion.contact.repository.IContactRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ContactController {
 
     private final IContactRepository provider;
 
-    public ContactController(IContactRepository provider) {
-        this.provider = provider;
-    }
-
-    @GetMapping("/")
+    @GetMapping("/contacts")
     public String list(Model model) {
         model.addAttribute("contacts", provider.findAll());
         model.addAttribute("pageTitle", "Mes contacts");
